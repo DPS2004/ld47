e44ed81fd73a14becd66a44170d378fb117a3a6e
DeadlySprinklez <gameboy.alex123@gmail.com>
2020-10-05 06:39:10 -0700

    trash the particle emitter, instead make color rectangle

--- rooms/menu.lua
+++ rooms/menu.lua
@@ -3,12 +3,16 @@ Menu = Room:extend()
 function Menu:new()
   self.super.new(self)
   love.graphics.setFont(game_font)
-  self.emitter = love.graphics.newParticleSystem(particle, 100)
-  self.emitter:setDirection(math.rad(90))
-  self.emitter:setSpread(math.rad(45))
-  self.emitter:setSpeed(5,10)
-  self.emitter:setLinearDamping(0,1)
-  self.emitter:setParticleLifetime(5,5)
+  self.last = 0
+  self.color = {1,1,1,0}
+  self.colors = {
+    {{1,1,1,0}}, --default
+    {{1,0,77/255,1},{1,163/255,0,1},{1,236/255,39/255,1},{0,228/255,54/255,1},{41/255,173/255,1,1},{131/255,118/255,156/255,1},{1,119/255,168/255,1}}, --dps
+    {{72/255,72/255,92/255,1},{130/255,84/255,113/255,1},{1,69/255,0,1}}, --pipe
+    {{124/255,30/255,1,1},{1,41/255,125/255,1}}, --katie
+    {{1,0,0,1},{1,0.5,0,1},{1,1,0,1},{0,1,0,1},{0,0,1,1},{0.1,0,1,1},{1,0,1,1},{1,1,1,1}} --ds
+  }
+  self.focus = 1
 
   self.optionsMenu = self:addObject(OptionBox, gw/2, gh/2, {width = 120, height = 46, options = {
     {type = "function", name = "Play", func = function() gotoRoom('Stage') end},
@@ -24,29 +28,35 @@ function Menu:new()
     {type = "function", name = "Credits", func = function ()
       self.optionsMenu:kill()
       self:addObject(OptionBox, gw/2, gh/2, {width = 200, height = 66, options = {
-        {type = "function", name = "DPS2004 - Coordinator, Lead Developer", func = function() end},
-        {type = "function", name = "pipelinks - Programmer, Shader Expert", func = function() end},
-        {type = "function", name = "Katie1118 - Lead Artist, Hypesquad", func = function() end},
-        {type = "function", name = "DeadlySprinklez - Programmer, Artist", func = function() 
-          self.emitter:setColors({1,0,0,1}, {1,0.5,0,1}, {1,1,0,1}, {0,1,0,1}, {0,0,1,0}, {0.1,0,1,1}, {1,0,1,1}, {1,1,1,1})
-          self.emitter:start()
-        end},
-        {type = "function", name = ("" or (emitter:isActive() and "Stop Particles")), func = function() self.emitter:stop() end},
+        {type = "function", name = "DPS2004 - Coordinator, Lead Developer", func = function() self.focus = 2 end},
+        {type = "function", name = "pipelinks - Programmer, Shader Expert", func = function() self.focus = 3 end},
+        {type = "function", name = "Katie1118 - Lead Artist, Hypesquad", func = function() self.focus = 4 end},
+        {type = "function", name = "DeadlySprinklez - Programmer, Artist", func = function() self.focus = 5 end},
+        {type = "function", name = "", func = function() self.focus = 1 end},
         {type = "function", name = "Back", func = function() self:new() end}
       }, selected_index = 6})
     end},
     {type = "function", name = "Exit", func = function() love.event.quit() end},
   }})
-  self.emitter:setPosition(self.optionsMenu.x + self.optionsMenu.padding - self.optionsMenu.width / 2, gh/2)
 end
 
 function Menu:update(dt)
-  self.super.update(dt)
-  self.emitter:update(dt)
+  self.timer:update(dt)
+  Menu.super.update(self, dt)
+  if self.last+0.5 < love.timer.getTime() then
+    self.color = self.colors[self.focus][math.random(1,#self.colors[self.focus])]
+    self.last = love.timer.getTime()
+  end
 end
 
 function Menu:draw()
   push:setCanvas("main")
   self.super.draw(self)
   helpers.color(1)
+  love.graphics.push("all")
+  love.graphics.setColor(self.color)
+  love.graphics.rectangle("fill",0,gh-18,gw,18,4)
+  love.graphics.pop()
+  helpers.color(2)
+  love.graphics.print("Thanks for playing! Here's some colors chosen by the devs.", 26, gh-13)
 end
