6b5cc3d794771a8985c89ed0118ad49e51450712
DeadlySprinklez <gameboy.alex123@gmail.com>
2020-10-05 05:01:59 -0700

    begin particle emitter easter egg attempt

--- rooms/menu.lua
+++ rooms/menu.lua
@@ -3,6 +3,12 @@ Menu = Room:extend()
 function Menu:new()
   self.super.new(self)
   love.graphics.setFont(game_font)
+  self.emitter = love.graphics.newParticleSystem(particle, 100)
+  self.emitter:setDirection(math.rad(90))
+  self.emitter:setSpread(math.rad(45))
+  self.emitter:setSpeed(5,10)
+  self.emitter:setLinearDamping(0,1)
+  self.emitter:setParticleLifetime(5,5)
 
   self.optionsMenu = self:addObject(OptionBox, gw/2, gh/2, {width = 120, height = 46, options = {
     {type = "function", name = "Play", func = function() gotoRoom('Stage') end},
@@ -21,14 +27,23 @@ function Menu:new()
         {type = "function", name = "DPS2004 - Coordinator, Lead Developer", func = function() end},
         {type = "function", name = "pipelinks - Programmer, Shader Expert", func = function() end},
         {type = "function", name = "Katie1118 - Lead Artist, Hypesquad", func = function() end},
-        {type = "function", name = "DeadlySprinklez - Programmer, Artist", func = function() end},
-        {type = "function", name = "", func = function() end},
+        {type = "function", name = "DeadlySprinklez - Programmer, Artist", func = function() 
+          self.emitter:setColors({1,0,0,1}, {1,0.5,0,1}, {1,1,0,1}, {0,1,0,1}, {0,0,1,0}, {0.1,0,1,1}, {1,0,1,1}, {1,1,1,1})
+          self.emitter:start()
+        end},
+        {type = "function", name = ("" or (emitter:isActive() and "Stop Particles")), func = function() self.emitter:stop() end},
         {type = "function", name = "Back", func = function() self:new() end}
       }, selected_index = 6})
     end},
     {type = "function", name = "Exit", func = function() love.event.quit() end},
   }})
-end 
+  self.emitter:setPosition(self.optionsMenu.x + self.optionsMenu.padding - self.optionsMenu.width / 2, gh/2)
+end
+
+function Menu:update(dt)
+  self.super.update(dt)
+  self.emitter:update(dt)
+end
 
 function Menu:draw()
   self.super.draw(self)
